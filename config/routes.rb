3c270d3e63d5d5bcae5a82875104ec2303ec172b
BaburMB <babur.mb@live.com>
2015-04-21 14:29:37 +0600

    updated routes, added bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -7,18 +7,18 @@ Rails.application.routes.draw do
 
   get 'toy/Contacts'
 
-  get 'users/index'
-
-  get 'users/new'
-
-  get 'users/edit'
+  # get 'users/index'
+  #
+  # get 'users/new'
+  #
+  # get 'users/edit'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-    root 'toy#Home'
-    get 'users#index'
+  root 'toy#Home'
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
